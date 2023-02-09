dad98566cfcd5bbecb93839faaf79a71575c95dd
WildZes <wildzes@gmail.com>
2023-02-09 23:49:58 +0300

    Create business.py

---
+++ botWithMentor/business.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+# инициализация декларативного стиля
+Base = declarative_base()
+
+
+class Business(Base):
+
+    __tablename__ = 'business'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    entrepreneur_id = Column(Integer, ForeignKey('entrepreneur.id'))
+    entrepreneur = relationship(
+        Entrepreneur,
+        backref=backref('entrepreneur',
+                        uselist=True,
+                        cascade='delete-orphan, all'))
+
+    def __str__(self):
+        return self.name
