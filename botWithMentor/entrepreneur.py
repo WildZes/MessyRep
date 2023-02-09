03e08b8266a9f973f9714721980473be9753ee0c
WildZes <wildzes@gmail.com>
2023-02-09 23:45:01 +0300

    Create entrepreneur.py

---
+++ botWithMentor/entrepreneur.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+# инициализация декларативного стиля
+Base = declarative_base()
+
+
+class Entrepeneur(Base):
+  
+  __tablename__ = 'entrepreneurs'
+  
+  id = Column(Integer, primary_key=True)
+  name = Column(String, index=True, unique=True)
+  
+  def __str__(self):
+    return self.name
